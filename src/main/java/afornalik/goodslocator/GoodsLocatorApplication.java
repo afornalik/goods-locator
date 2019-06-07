1e8e77c0bc810fe590ae59852277742e934ff7b6
Andrzejf <afornalik2@gmail.com>
2019-06-08 00:00:18 +0200

    initial commit

---
+++ src/main/java/afornalik/goodslocator/GoodsLocatorApplication.java
@@ -0,0 +1,13 @@
+package afornalik.goodslocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsLocatorApplication.class, args);
+	}
+
+}
