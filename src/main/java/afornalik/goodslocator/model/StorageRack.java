afd088a3413efce9550c23a9bd41876f747faf16
Andrzejf <afornalik2@gmail.com>
2019-06-09 23:16:41 +0200

    change spring version to 2.1.4

---
+++ src/main/java/afornalik/goodslocator/model/StorageRack.java
@@ -0,0 +1,27 @@
+package afornalik.goodslocator.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "storage_rack")
+public class StorageRack {
+	
+	@Id
+	@GeneratedValue (strategy=GenerationType.IDENTITY )
+	private int id;
+
+	private int rows;
+	
+	private int collumns;
+	
+	private String description;
+}
