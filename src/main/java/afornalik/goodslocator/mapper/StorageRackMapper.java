aaa2fe1c47b8f50d3b144361705481b2261d910a
Andrzejf <afornalik2@gmail.com>
2019-06-10 23:27:26 +0200

    add StorageRackMapper tests

--- src/main/java/afornalik/goodslocator/mapper/StorageRackMapper.java
+++ src/main/java/afornalik/goodslocator/mapper/StorageRackMapper.java
@@ -2,25 +2,30 @@ package afornalik.goodslocator.mapper;
 
 import org.springframework.stereotype.Component;
 
-import afornalik.goodslocator.exception.StorageRackIdException;
+import afornalik.goodslocator.exception.StorageRackConvertException;
 import afornalik.goodslocator.model.StorageRack;
 import afornalik.goodslocator.modeldto.StorageRackDTO;
 
 @Component
 public class StorageRackMapper {
 
-	public StorageRack convertToEntity(StorageRackDTO dto) {
-		try {
+	public StorageRack convertToEntity(StorageRackDTO dto) throws StorageRackConvertException {
+		
 			if (dto.getId() != 0) {
 				StorageRack rack = new StorageRack(dto.getId(), dto.getRows(), dto.getCollumns(), dto.getDescription());
 				return rack;
 			} else {
-				throw new StorageRackIdException();
+				throw new StorageRackConvertException();
 			}
-		} catch (StorageRackIdException rackIdException) {
-			return null;
-		}
 		
+
+	}
+
+	public StorageRackDTO convertToDTO(StorageRack storageRack) {
+		StorageRackDTO storageRackDTO = new StorageRackDTO(storageRack.getId(), storageRack.getRows(),
+				storageRack.getCollumns(), storageRack.getDescription());
+		
+		return storageRackDTO;
 	}
 
 }
--- src/test/java/afornalik/goodslocator/StorageRackMapperTest.java
+++ src/test/java/afornalik/goodslocator/StorageRackMapperTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import afornalik.goodslocator.exception.StorageRackConvertException;
 import afornalik.goodslocator.mapper.StorageRackMapper;
 import afornalik.goodslocator.model.StorageRack;
 import afornalik.goodslocator.modeldto.StorageRackDTO;
@@ -18,17 +19,18 @@ import afornalik.goodslocator.modeldto.StorageRackDTO;
 public class StorageRackMapperTest {
 	
 	private StorageRackDTO storageRackDTO;
+	private StorageRack storageRack; 
 	
 	@Autowired
 	private StorageRackMapper storageMapper;
 	
 	@Test
-	public void shouldMapDTOtoEntity() {
+	public void shouldMapDTOtoEntity() throws StorageRackConvertException {
 		//given
 		storageRackDTO = new StorageRackDTO(5, 10, 15, "some dummy description");
 		
 		//when
-		StorageRack storageRack = storageMapper.convertToEntity(storageRackDTO);
+		storageRack = storageMapper.convertToEntity(storageRackDTO);
 		
 		//then
 		Assert.assertEquals(storageRack.getId(), storageRackDTO.getId());
@@ -37,4 +39,28 @@ public class StorageRackMapperTest {
 		Assert.assertEquals(storageRack.getDescription(), storageRackDTO.getDescription());
 	}
 
+	
+	@Test
+	public void shouldMapEntityFromDTO() {
+		//given
+		storageRack = new StorageRack(11,77,22,"description");
+		
+		//when
+		storageRackDTO = storageMapper.convertToDTO(storageRack);
+		
+		//then
+		Assert.assertEquals(storageRack.getId(), storageRackDTO.getId());
+		Assert.assertEquals(storageRack.getRows(), storageRackDTO.getRows());
+		Assert.assertEquals(storageRack.getCollumns(), storageRackDTO.getCollumns());
+		Assert.assertEquals(storageRack.getDescription(), storageRackDTO.getDescription());
+		}
+	
+	@Test(expected = StorageRackConvertException.class)
+	public void shouldThrowStorageRackConvertException() throws StorageRackConvertException {
+		//given
+		storageRackDTO = new StorageRackDTO(0,77,22,"description");
+		
+		//when
+		storageRack = storageMapper.convertToEntity(storageRackDTO);
+	}
 }
